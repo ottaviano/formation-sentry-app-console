60aedb2052efe459e69cc26b9941ac0b206174a9
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2018-10-16 22:22:31 +0200

    commit 2

--- src/script.php
+++ src/script.php
@@ -4,10 +4,10 @@ ini_set('error_reporting', 0);
 
 require __DIR__.'/../vendor/autoload.php';
 
-$sentryClient = new Raven_Client('http://471a12edc4ce4db2bc39d270945a1516@192.168.99.100:9000/2');
+$sentryClient = new Raven_Client('https://a05f751d864441cba9c3e710d6df9476@sentry.io/1302553');
 $sentryClient->install();
 
-$sentryClient->setRelease('v1.0.0');
+$sentryClient->setRelease('2b779d0714a0d8a1e3f2fb069465c06e5da1901b');
 
 $sentryClient->setEnvironment('dev');
 $sentryClient->setEnvironment('preprod');
@@ -23,6 +23,7 @@ $user->birthDate = new \DateTime('1989-03-01');
 $user->password = 'test';
 
 function calculate_age(object $user, \DateTime $date): int {
+    return null;
     return abs($date->diff($user->birthDate)->y);
 }
 
